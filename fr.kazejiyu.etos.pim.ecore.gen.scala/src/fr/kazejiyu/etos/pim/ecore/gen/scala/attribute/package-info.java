dfa1055383ef610dde0509772d75e8d6381bc792
Emmanuel CHEBBI <emmanuel.chebbi@outlook.fr>
2017-11-18 15:41:38 +0100

    Move templates to gather them by concerns.
    Templates must be grouped according to the Scala element they generate :
    class, trait, method, attribute, etc.

---
+++ fr.kazejiyu.etos.pim.ecore.gen.scala/src/fr/kazejiyu/etos/pim/ecore/gen/scala/attribute/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Acceleo templates making able to generate a Scala class' attribute.
+ * 
+ * @author Emmanuel CHEBBI
+ */
+package fr.kazejiyu.etos.pim.ecore.gen.scala.attribute;
---
+++ fr.kazejiyu.etos.pim.ecore.gen.scala/src/fr/kazejiyu/etos/pim/ecore/gen/scala/method/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Acceleo templates making able to generate a Scala method.
+ * 
+ * @author Emmanuel CHEBBI
+ */
+package fr.kazejiyu.etos.pim.ecore.gen.scala.method;
